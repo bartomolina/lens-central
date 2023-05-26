9403c3f9a7687b66c15e0110658da2fc6bae8d35
Barto Molina <bartomolina@gmail.com>
2023-05-26 19:54:47 +0200

    fix: linter issues

--- lib/get-picture-proxy-url.ts
+++ lib/get-picture-proxy-url.ts
@@ -1,7 +1,7 @@
 export const getPictureProxyURL = (url: string | undefined) => {
   let picture: string;
   if (url && url.startsWith("ipfs://")) {
-    const result = url.substring(7, url.length);
+    const result = url.slice(7, url.length);
     picture = `https://lens.infura-ipfs.io/ipfs/${result}`;
   } else {
     picture = "/default_profile.jpeg";
